f3fe617f4f606b30ee860fe0b80848e9b07f8679
timiambaye <itstimiking@gmail.com>
2024-06-02 22:57:48 +0100

    Fix

--- paymentproject/settings.py
+++ paymentproject/settings.py
@@ -139,13 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = '22'
-# EMAIL_HOST_USER = 'itstimiking@gmail.com'
-# EMAIL_HOST_PASSWORD = 'mdwpq5ca'
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
